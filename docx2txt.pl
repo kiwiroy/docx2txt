d51a3cac7629063515dd9123b762c5f01978630f
idiotkid <idiotkid>
2009-09-05 21:16:47 +0000

    documentation updates to make v0.4 release.

--- docx2txt.pl
+++ docx2txt.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 
 # docx2txt, a command-line utility to convert Docx documents to text format.
-# Copyright (C) 2008 Sandeep Kumar
+# Copyright (C) 2008-2009 Sandeep Kumar
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
